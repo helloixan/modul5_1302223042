fa20f1783489ef197692d7ac265a75d8536a416f
helloixan <iksanrisandy@gmail.com>
2024-03-21 10:30:09 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<float> dataBase = new SimpleDataBase<float>();
+        dataBase.AddNewData(13);
+        dataBase.AddNewData(02);
+        dataBase.AddNewData(22);
+        dataBase.PrintAllData();
+    }
+}
+
+
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        this.storedData.Add(data);
+        this.inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < this.storedData.Count; i++)
+        {
+            String date = inputDates[i].ToString("dd/MM/yyyy hh:mm:ss tt");
+            Console.WriteLine("Data " + (i+1) + " berisi: " + storedData[i] + " yang disimpan pada waktu UTC: " + date);
+        }
+    }
+}
--- obj/Debug/net8.0/modul5_1302223042.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_1302223042.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_1302223042")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de651c58de50a4afdd779b19a1c0a8d1c562104d")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_1302223042")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_1302223042")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
